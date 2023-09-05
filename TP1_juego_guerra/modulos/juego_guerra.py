2fdd7963d70586315ee961c1959fa4c84efae9e7
sabrivilche <sabrinavilche18@gmail.com>
2023-09-05 20:36:42 -0300

    Juego Guerra

--- TP1_juego_guerra/modulos/juego_guerra.py
+++ TP1_juego_guerra/modulos/juego_guerra.py
@@ -29,28 +29,43 @@ class JuegoGuerra:
         else:
             self.iniciar_guerra()
 
-        def iniciar_guerra(self):
-            cartas_en_mesa=[self.mazo_jugador1.sacar_carta(),self.mazo_jugador2.sacar_carta()]
-            for _ in range(3):
-                if not self.mazo_jugador1.vacia():
-                    cartas_en_mesa.append(self.mazo_jugador1.sacar_carta())
-                
-                if not self.mazo_jugador2.vacia():
-                    cartas_en_mesa.append(self.mazo_jugador2.sacar_carta())
-                
-                carta_judador1=self.mazo_jugador1.sacar_carta if not self.mazo_jugador1.vacia() else None
-                carta_judador2=self.mazo_jugador2.sacar_carta if not self.mazo_jugador2.vacia() else None
+    def iniciar_guerra(self):
+        cartas_en_mesa=[self.mazo_jugador1.sacar_carta(),self.mazo_jugador2.sacar_carta()]
+        for _ in range(3):
+            if not self.mazo_jugador1.vacia():
+                cartas_en_mesa.append(self.mazo_jugador1.sacar_carta())
+            
+            if not self.mazo_jugador2.vacia():
+                cartas_en_mesa.append(self.mazo_jugador2.sacar_carta())
+            
+            carta_judador1=self.mazo_jugador1.sacar_carta if not self.mazo_jugador1.vacia() else None
+            carta_judador2=self.mazo_jugador2.sacar_carta if not self.mazo_jugador2.vacia() else None
 
-                if carta_judador1 is not None and carta_judador2 is not None:
-                    cartas_en_mesa.extend([carta_judador1,carta_judador2])
-                    
-                    if carta_judador1>carta_judador2:
-                        self.mazo_jugador1.agregar_carta(cartas_en_mesa)
-                    
-                    else:
-                        self.mazo_jugador2.agregar_carta(cartas_en_mesa)
+            if carta_judador1 is not None and carta_judador2 is not None:
+                cartas_en_mesa.extend([carta_judador1,carta_judador2])
+                
+                if carta_judador1>carta_judador2:
+                    self.mazo_jugador1.agregar_carta(cartas_en_mesa)
                 
                 else:
-                    if carta_judador1 is None: #Un jugador no tiene suficientes cartas para continuar, el otro gana
-                        self.ganador='Jugador 2'
-                        
+                    self.mazo_jugador2.agregar_carta(cartas_en_mesa)
+            
+            else:
+                if carta_judador1 is None: #Un jugador no tiene suficientes cartas para continuar, el otro gana
+                    self.ganador='Jugador 2'
+                else: 
+                    self.ganador='Jugador 1'
+                
+    def jugar_partida(self, max_turnos):
+        self.ganador=None
+        while not self.mazo_jugador1.vacia() and not self.mazo_jugador2.vacia() and self.turno<max_turnos:
+            self.jugar_turno()
+        
+        if self.ganador is None:
+            if self.mazo_jugador1.vacia:
+                self.ganador='Jugador 2'
+
+            else:
+                self.ganador='Jugador 1'
+
+        return self.turno, self.ganador
