ab2690bb66c48f8b575c2dd67520222408ef4e96
marianiz <mangelesniz@gmail.com>
2023-09-05 20:01:20 -0300

    cola

--- TP1_juego_guerra/modulos/cola.py
+++ TP1_juego_guerra/modulos/cola.py
@@ -1,18 +1,26 @@
+import random
 class Cola:
     def __init__(self):
-        self.datos = []
+        self.carta = []
         self.tamanio = 0
 
     def vacia(self):
-        return self.datos == None
+        return len(self.carta) == 0
     
-    def agregar_carta(self,dato):
-        self.datos.append(dato)
+    def agregar_carta(self,dato):#se agrega una sola carta(dato) a la cola
+        self.carta.append(dato)
         self.tamanio += 1
+    
+    def agregar_cartas(self,dato): #se agrega una lista de cartas al final de la cola self.carta
+        self.carta.extend(dato)
 
     def sacar_carta(self):
-        self.lanzar_carta = self.datos.pop()
+        self.lanzar_carta = self.carta.pop(0)
         return self.lanzar_carta
     
     def tamanio(self):
-        return len(self.datos)
+        return len(self.carta)
+    
+    def mezclar_mazo(self):
+        random.shuffle(self.carta)
+
