0dff2561dd45130f8cc389c0204b80d820805706
sabrivilche <sabrinavilche18@gmail.com>
2023-09-09 18:27:19 -0300

    Verificación de funcionamiento

--- TP1_ordenamiento_externo/modulos/generador_de_datos_10MB.py
+++ TP1_ordenamiento_externo/modulos/generador_de_datos_10MB.py
@@ -28,15 +28,16 @@ def crear_archivo_de_datos(nombre):
         print('t =', t, ', N_restantes =', N_restantes)
         bloque = [str(randint(10**(cif-1), 10**cif-1))+'\n'
                   for i in range(t)]        
-        with open(nombre, 'a+') as archivo:
+        with open("datos.txt", 'a+') as archivo:
             archivo.writelines(bloque)
             
 crear_archivo_de_datos('datos.txt')
 
 def mezcla_directa(nombre):
+    B=1000
     # Leer el archivo y dividirlo en bloques de B claves
     bloques = [] #almacenará los bloques de claves ordenadas
-    with open(nombre, 'r') as archivo: 
+    with open('datos.txt', 'r') as archivo: 
         bloque = [] #almacena las claves del bloque actual
         for linea in archivo:
             bloque.append(int(linea.strip())) #elimina los espacios en blanco, convierte los valores en enteros y los agrega a la lista bloque
@@ -47,7 +48,7 @@ def mezcla_directa(nombre):
             bloques.append(sorted(bloque)) #se ordenan las claves que quedaban y se agregan a la lista bloques
 
     # Ordenar los bloques y escribirlos en el archivo
-    with open(nombre, 'w') as archivo:
+    with open('datos.txt', 'w') as archivo:
         for bloque in bloques: #itera sobre cada bloque dentro de bloques
             for clave in bloque: #itera sobre cada clave dentro de bloque
                 archivo.write(str(clave) + '\n') #se escribe cada clave en el archivo como cadena de texto
@@ -55,7 +56,11 @@ def mezcla_directa(nombre):
 def verificar_ordenamiento(nombre):
     # Leer el archivo y verificar que las claves estén ordenadas
     claves = []
-    with open(nombre, 'r') as archivo:
+    with open('datos.txt', 'r') as archivo:
         for linea in archivo:
             claves.append(int(linea.strip()))
     return claves == sorted(claves) #lee las claves del archivo y las compara con una versión ordenada, si están ordenadas = True, de lo contrario = False
+
+
+#ordenar_archivo=mezcla_directa('datos.txt')
+#verificacion=verificar_ordenamiento(ordenar_archi)
