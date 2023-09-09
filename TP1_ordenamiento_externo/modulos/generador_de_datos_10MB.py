f488b09bac0a9d906d66a0edec7ca5d52df74098
marianiz <116604408+marianiz@users.noreply.github.com>
2023-09-08 21:19:00 -0300

    Update generador_de_datos_10MB.py

--- TP1_ordenamiento_externo/modulos/generador_de_datos_10MB.py
+++ TP1_ordenamiento_externo/modulos/generador_de_datos_10MB.py
@@ -35,22 +35,22 @@ crear_archivo_de_datos('datos.txt')
 
 def mezcla_directa(nombre):
     # Leer el archivo y dividirlo en bloques de B claves
-    bloques = []
-    with open(nombre, 'r') as archivo:
-        bloque = []
+    bloques = [] #almacenará los bloques de claves ordenadas
+    with open(nombre, 'r') as archivo: 
+        bloque = [] #almacena las claves del bloque actual
         for linea in archivo:
-            bloque.append(int(linea.strip()))
-            if len(bloque) == B:
-                bloques.append(sorted(bloque))
-                bloque = []
-        if bloque:
-            bloques.append(sorted(bloque))
+            bloque.append(int(linea.strip())) #elimina los espacios en blanco, convierte los valores en enteros y los agrega a la lista bloque
+            if len(bloque) == B: 
+                bloques.append(sorted(bloque)) #se ordena el bloque y se agrega a la lista bloques
+                bloque = [] #reinicio la lista bloque
+        if bloque: #si bloque no está vacío
+            bloques.append(sorted(bloque)) #se ordenan las claves que quedaban y se agregan a la lista bloques
 
     # Ordenar los bloques y escribirlos en el archivo
     with open(nombre, 'w') as archivo:
-        for bloque in bloques:
-            for clave in bloque:
-                archivo.write(str(clave) + '\n')
+        for bloque in bloques: #itera sobre cada bloque dentro de bloques
+            for clave in bloque: #itera sobre cada clave dentro de bloque
+                archivo.write(str(clave) + '\n') #se escribe cada clave en el archivo como cadena de texto
 
 def verificar_ordenamiento(nombre):
     # Leer el archivo y verificar que las claves estén ordenadas
@@ -58,4 +58,4 @@ def verificar_ordenamiento(nombre):
     with open(nombre, 'r') as archivo:
         for linea in archivo:
             claves.append(int(linea.strip()))
-    return claves == sorted(claves)
+    return claves == sorted(claves) #lee las claves del archivo y las compara con una versión ordenada, si están ordenadas = True, de lo contrario = False
