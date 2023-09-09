e40f3e864234e4e2258cf9c0381e01362aa09546
marianiz <116604408+marianiz@users.noreply.github.com>
2023-09-08 21:04:59 -0300

    Update juego_guerra.py

--- TP1_juego_guerra/modulos/juego_guerra.py
+++ TP1_juego_guerra/modulos/juego_guerra.py
@@ -5,7 +5,7 @@ class Carta:
     def __init__(self, valor, palo):
         self.valor = valor
         self.palo = palo
-        self.boca_arriba = False
+        self.boca_arriba = False #considero que todas las cartas están boca abajo cuando comienza el juego
 
     def __str__(self):
         return f"{self.valor}{self.palo}"
@@ -40,32 +40,32 @@ class JuegoGuerra:
         self.turno = 0
         self.max_turnos = 10000  # Número máximo de turnos para evitar bucles infinitos
 
-        random.seed(semilla)
+        random.seed(semilla) #genero números aleatorios
 
     def iniciar_juego(self):
-        while not self.cartas_jugador1.esta_vacio() and not self.cartas_jugador2.esta_vacio() and self.turno < self.max_turnos:
+        while not self.cartas_jugador1.esta_vacio() and not self.cartas_jugador2.esta_vacio() and self.turno < self.max_turnos: #mientras ninguno de los 2 jugadores se quede sin cartas y no se pasen del número de turnos
             self.turno += 1
-            carta_jugador1 = self.cartas_jugador1.poner_sacar_arriba()
+            carta_jugador1 = self.cartas_jugador1.poner_sacar_arriba() #se saca la carta de arriba del mazo de cada jugador y se almacena en carta_jugador
             carta_jugador2 = self.cartas_jugador2.poner_sacar_arriba()
 
             # Comparar las cartas
-            if self.comparar_cartas(carta_jugador1, carta_jugador2):
+            if self.comparar_cartas(carta_jugador1, carta_jugador2): #si la comparación la gana el jugador 1 las cartas se ponen en la parte inferior del mazo
                 self.cartas_jugador1.poner_abajo(carta_jugador1)
                 self.cartas_jugador1.poner_abajo(carta_jugador2)
-            else:
+            else: #si la comparación la gana el jugador 2
                 self.cartas_jugador2.poner_abajo(carta_jugador1)
                 self.cartas_jugador2.poner_abajo(carta_jugador2)
 
         if self.turno == self.max_turnos:
-            return "Empate"
+            return "Empate" #si se alcanza la cantidad límite de turnos se considera un empate
         elif self.cartas_jugador1.esta_vacio():
-            return "Jugador 2"
+            return "Jugador 2" #si el mazo del jugador 1 está vacio se considera como ganador el jugador 2
         else:
             return "Jugador 1"
 
     def comparar_cartas(self, carta1, carta2):
         valores_cartas = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        valor_carta1 = valores_cartas.index(carta1.valor)
+        valor_carta1 = valores_cartas.index(carta1.valor) #se comparan los valores de cada carta
         valor_carta2 = valores_cartas.index(carta2.valor)
-        return valor_carta1 > valor_carta2
+        return valor_carta1 > valor_carta2 #si la carta1 es mayor que la carta2 retorna True, de lo contrario False
 
