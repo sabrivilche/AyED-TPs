60de1e6fa86e0f386e39ce0a48b97c9b48205640
marianiz <116604408+marianiz@users.noreply.github.com>
2023-09-09 18:54:16 -0300

    Update Nodo.py

--- TP1_LDE_testing/Nodo.py
+++ TP1_LDE_testing/Nodo.py
@@ -1,5 +1,5 @@
 class Nodo:
   def __init__(self,dato):
-    self.dato = dato
-    self.anterior = None
-    self.siguiente = None
+    self.dato = dato #será el valor almacenado en el nodo.
+    self.anterior = None #referenciado al nodo anterior en la lista doblemente enlazada.
+    self.siguiente = None #nodo siguiente en la lista doblemente enlazada.
