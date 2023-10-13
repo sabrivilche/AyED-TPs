701866efa14a36c690a547917c0305203780e68e
sabrivilche <sabrinavilche18@gmail.com>
2023-10-13 18:40:56 -0300

    Precio minimo

--- TP2_problema3/modulo.py
+++ TP2_problema3/modulo.py
@@ -29,9 +29,9 @@ class Vertice:
         self.conectadoA = {}
         self.distancia = 0
         self.predecesor = None
-
-    def agregarVecino(self, vecino, ponderacion=0):
-        self.conectadoA[vecino] = ponderacion
+        self.precio=float('inf')#Agrego atributo precio
+    def agregarVecino(self, vecino, ponderacion=0,precio=0):
+        self.conectadoA[vecino] = (ponderacion,precio)#Guardamos el precio junto con la ponderación
 
     def asignarDistancia(self, distancia):
         self.distancia = distancia
@@ -52,8 +52,10 @@ class Vertice:
         return self.conectadoA.keys()
 
     def obtenerPonderacion(self, vecino):
-        return self.conectadoA[vecino]
+        return self.conectadoA[vecino][0]
 
+    def obtenerPrecio(self,vecino):
+        return self.conectadoA[vecino][1]
 class Grafo:
     def __init__(self):
         self.listaVertices = {}
@@ -74,12 +76,12 @@ class Grafo:
     def __contains__(self, n):
         return n in self.listaVertices
 
-    def agregarArista(self, de, a, costo=0):
+    def agregarArista(self, de, a, costo=0,precio=0):
         if de not in self.listaVertices:
             nv = self.agregarVertice(de)
         if a not in self.listaVertices:
             nv = self.agregarVertice(a)
-        self.listaVertices[de].agregarVecino(self.listaVertices[a], costo)
+        self.listaVertices[de].agregarVecino(self.listaVertices[a], costo,precio)
 
     def obtenerVertices(self):
         return self.listaVertices.keys()
@@ -91,6 +93,7 @@ def dijkstra_max_bottleneck(graph, start, end):
     cp = ColaPrioridad()
     start_vertex = graph.obtenerVertice(start)
     start_vertex.asignarDistancia(float('inf'))
+    start_vertex.precio=0#Iniciamos precio a 0 para el vertice inicial
     cp.insertar(start_vertex, -start_vertex.obtenerDistancia())
 
     while not cp.esta_vacia():
@@ -98,16 +101,19 @@ def dijkstra_max_bottleneck(graph, start, end):
 
         for vertex_siguiente in vertex_actual.obtenerConexiones():
             nueva_distancia = min(vertex_actual.obtenerDistancia(), vertex_actual.obtenerPonderacion(vertex_siguiente))
-
+            nuevo_precio=vertex_actual.precio+vertex_actual.obtenerPrecio(vertex_siguiente)#Sumamos el precio de la arista
+            
             if nueva_distancia > vertex_siguiente.obtenerDistancia():
                 vertex_siguiente.asignarDistancia(nueva_distancia)
+                vertex_siguiente.precio=nuevo_precio#Actualizamos nuevo precio
                 vertex_siguiente.asignarPredecesor(vertex_actual)
                 cp.decrementar_clave(vertex_siguiente, -nueva_distancia)
-
+                
+   
     end_vertex = graph.obtenerVertice(end)
     max_bottleneck = end_vertex.obtenerDistancia()
-
-    return max_bottleneck
+    min_price=end_vertex.precio#Obtenemos el precio minimo
+    return max_bottleneck,min_price
 
 
 if __name__ == "__main__":
@@ -121,16 +127,31 @@ if __name__ == "__main__":
         for line in file:
             start, end, capacity, price = line.strip().split(',')
             capacity = int(capacity)
+            price=int(price)
             ciudades.add(start)
             ciudades.add(end)
-            graph.agregarArista(start, end, capacity)
+            graph.agregarArista(start, end, capacity,price)
 
-    start_city = "CiudadBs.As."
+    start_city = "CiudadBs.As."#se establece ciudad de incio para el cálculo del camino más corto
+    #Se inicializamn variables que almacenaarán la ciudad con el mayor peso maximo(cuello botella), el peso max y el precio min 
+    max_bottleneck_city = None
+    max_bottleneck_weight = 0
+    min_price_for_max_bottleneck = float('inf')
 
-    # Calcular el mayor peso desde bs as a la otra ciudad
     for end_city in ciudades:
         if start_city != end_city:
-            max_bottleneck = dijkstra_max_bottleneck(graph, start_city, end_city)
-            print(f"El peso máximo que se puede transportar desde {start_city} a {end_city} es {max_bottleneck} kg.")
+            max_bottleneck, min_price = dijkstra_max_bottleneck(graph, start_city, end_city)
+            if max_bottleneck > max_bottleneck_weight or (max_bottleneck == max_bottleneck_weight and min_price < min_price_for_max_bottleneck):
+                max_bottleneck_city = end_city
+                max_bottleneck_weight = max_bottleneck
+                min_price_for_max_bottleneck = min_price
+
+    print(f"La ciudad con el mayor peso máximo desde {start_city} es {max_bottleneck_city} con un peso de {max_bottleneck_weight} kg y con el menor precio de {min_price_for_max_bottleneck}.")
+    # Calcular el mayor peso desde bs as a la otra ciudad
+    #for end_city in ciudades:
+     #   if start_city != end_city:
+      #      max_bottleneck,min_price = dijkstra_max_bottleneck(graph, start_city, end_city)
+
+       #     print(f"El peso máximo que se puede transportar desde {start_city} a {end_city} es {max_bottleneck} kg. Precio: {min_price}")
 
 
